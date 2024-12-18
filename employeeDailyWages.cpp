d9dc56e47296b8bd828d6e25df00794aab9fff44
shivamrajput18 <shivamsingh80094@gmail.com>
2024-12-18 15:12:05 +0530

    Add:Calculating DailyWages

---
+++ employeeDailyWages.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+      const int wagePerHour = 20;
+      const int fullDayHour = 8;
+      
+      int dailyWage = wagePerHour*fullDayHour;
+
+
+      cout << " Daily Employee Wage : " << dailyWage << endl;
+
+    return 0;
+}
